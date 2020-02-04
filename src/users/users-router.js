96f95be059e254dc2b1e8c6be6d83ddbfc53afde
thanneman <thanneman1@gmail.com>
2020-02-03 20:59:24 -0700

    added user router

---
+++ src/users/users-router.js
@@ -0,0 +1,218 @@
+const express = require('express')
+const xss = require('xss')
+const path = require('path')
+const usersRouter = express.Router()
+const jsonBodyParser = express.json()
+const UsersService = require('./users-service')
+const GamesService = require('../games/games-service')
+const { requireAuth } = require('../middleware/jwt-auth')
+
+const jsonParser = express.json()
+
+const serializeUser = user => ({
+    user_id: user.id,
+    email: xss(user.email),
+    password: xss(user.password),
+    date_created: new Date(user.date_created),
+})
+
+const serializeGame = game => ({
+    id: game.id,
+    course_name: game.course_name,
+    date: game.date,
+    course_par: game.course_par,
+    front_score: game.front_score,
+    back_score: game.back_score,
+    notes: xss(game.notes),
+  })
+
+// All users
+usersRouter
+    .route('/')
+    .get((req, res, next) => {
+        UsersService.getAllUsers(req.app.get('db'))
+            .then(users => {
+                res.json(users.map(serializeUser))
+            })
+            .catch(next)
+    })
+    .post(jsonBodyParser, (req, res, next) => {
+        const { email, password } = req.body
+        for (const field of ['email', 'password'])
+            if (!req.body[field])
+                return res.status(400).json({
+                    error: `Missing '${field}' in request body`
+                })
+        const passwordError = UsersService.validatePassword(password)
+
+        if (passwordError)
+            return res.status(400).json({ error: passwordError })
+
+        UsersService.hasUserWithUserName(
+            req.app.get('db'),
+            email
+        )
+            .then(hasUserWithUserName => {
+                if (hasUserWithUserName)
+                    return res.status(400).json({ error: `Username already taken` })
+
+                return UsersService.hashPassword(password)
+                    .then(hashedPassword => {
+                        const newUser = {
+                            email,
+                            password: hashedPassword,
+                        }
+                        return UsersService.insertUser(
+                            req.app.get('db'),
+                            newUser
+                        )
+                            .then(user => {
+                                res
+                                    .status(201)
+                                    .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                                    .json(UsersService.serializeUser(user))
+                            })
+                    })
+            })
+            .catch(next)
+    })
+
+// Individual users by id
+usersRouter
+    .route('/:user_id')
+    .all((req, res, next) => {
+        const { user_id } = req.params;
+        UsersService.getById(req.app.get('db'), user_id)
+            .then(user => {
+                if (!user) {
+                    return res
+                        .status(404)
+                        .send({ error: { message: `User does not exist.` } })
+                }
+                res.user = user
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res) => {
+        res.json(UsersService.serializeUser(res.user))
+    })
+    .delete((req, res, next) => {
+        const { user_id } = req.params;
+        UsersService.deleteUser(
+            req.app.get('db'),
+            user_id
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+// Individual user, all games -- GET (all), POST, DELETE
+usersRouter
+    .route('/:user_id/games')
+    .all(requireAuth)
+    .all((req, res, next) => {
+        const { user_id } = req.params;
+        UsersService.getGamesById(req.app.get('db'), user_id)
+            .then(game => {
+                if (!game) {
+                    return res
+                        .status(404).json({ error: { message: `No games exist.` } })
+                }
+                res.game = game
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res) => {
+        res.json(res.game)
+    })
+    .post(requireAuth, jsonParser, (req, res, next) => {
+        const { course_name, course_par, front_score, back_score } = req.body
+        const newGame = { course_name, course_par, front_score, back_score }
+
+        for (const [key, value] of Object.entries(newGame))
+            if (value == null)
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` }
+                })
+
+        newGame.user_id = req.user.id
+
+        GamesService.insertUserGame(
+            req.app.get('db'),
+            newGame
+        )
+            .then(game => {
+                res
+                    .status(201)
+                    .json(serializeGame(game))
+            })
+            .catch(next)
+    })
+    .delete((req, res, next) => {
+        const { user_id } = req.params;
+        UsersService.deleteUser(
+            req.app.get('db'),
+            user_id
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+// Individual user, individual game -- GET (game by id), DELETE (game by id)
+usersRouter
+    .route('/:user_id/game/:game_id')
+    .all(requireAuth)
+    .all((req, res, next) => {
+        GamesService.getById(
+            req.app.get('db'),
+            req.params.game_id
+        )
+            .then(game => {
+                if (!game) {
+                    return res.status(404).json({
+                        error: { message: `Game does not exist` }
+                    })
+                }
+                res.game = game
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res) => {
+        res.json(res.game)
+    })
+    .delete((req, res, next) => {
+        GamesService.deleteGame(
+            req.app.get('db'),
+            req.params.game_id
+        )
+            .then(() => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+    /*
+    .patch(jsonParser, (req, res, next) => {
+        const { course_name, course_par, front_score, back_score } = req.body
+        const gameToUpdate = { course_name, course_par, front_score, back_score }
+
+        GamesService.updateGame(
+            req.app.get('db'),
+            req.params.game_id,
+            gameToUpdate
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+    */
+
+module.exports = usersRouter
---
+++ src/users/users-service.js
@@ -0,0 +1,80 @@
+const xss = require('xss')
+const bcrypt = require('bcryptjs')
+
+const REGEX_UPPER_LOWER_NUMBER = /(?=.*[a-z])(?=.*[A-Z])[\S]+/
+
+const UsersService = {
+    getAllUsers(knex) {
+        return knex.select('*').from('users')
+    },
+    hasUserWithUserName(db, email) {
+        return db('users')
+            .where({ email })
+            .first()
+            .then(user => !!user)
+    },
+    insertUser(db, newUser) {
+        return db
+            .insert(newUser)
+            .into('users')
+            .returning('*')
+            .then(([user]) => user)
+    },
+    validatePassword(password) {
+        if (password.length < 6) {
+            return 'Password must be longer than 6 characters'
+        }
+        if (password.length > 20) {
+            return 'Password must be less than 20 characters'
+        }
+        if (password.startsWith(' ') || password.endsWith(' ')) {
+            return 'Password must not start or end with empty spaces'
+        }
+        if (!REGEX_UPPER_LOWER_NUMBER.test(password)) {
+            return 'Password must contain 1 upper case, lower case, and a number'
+        }
+        return null
+    },
+    hashPassword(password) {
+        return bcrypt.hash(password, 12)
+    },
+    serializeUser(user) {
+        return {
+            id: user.id,
+            email: xss(user.email),
+            date_created: new Date(user.date_created),
+        }
+    },
+    serializeGame(game){
+        return {
+        id: game.id,
+        course_name: game.course_name,
+        date: game.date,
+        course_par: game.course_par,
+        front_score: game.front_score,
+        back_score: game.back_score,
+        notes: xss(game.notes),
+        }
+    },
+    deleteUser(knex, id) {
+        return knex
+            .from('users')
+            .where({ id })
+            .delete()
+    },
+    getById(knex, id) {
+        return knex
+            .from('users')
+            .select('*')
+            .where('id', id)
+            .first()
+    },
+    getGamesById(knex, id) {
+        return knex
+            .from('games')
+            .select('*')
+            .where('user_id', id)
+    },
+}
+
+module.exports = UsersService
